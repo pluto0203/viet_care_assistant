dddbc28c7b2de50dba8366b0a568a3b15ca710d3
pluto0203 <duynvtse171447@fpt.edu.vn>
2025-09-29 14:37:58 +0700

    create kb for RAG

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from app.models.user import User, Roles
 from app.models.message import Message
 from app.models.conversation import Conversation
+from app.models.knowledge_base import KBFAQ, KBFAQEmbedding, KBCollection, Language
